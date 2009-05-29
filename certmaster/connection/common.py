6076585f841f3bf1638468b763aa6575b02f7d9c
makkalot <makkalot@fedorabig.localdomain>
2009-05-29 14:56:09 +0300

    forcing connection good for testing purposes

--- certmaster/connection/common.py
+++ certmaster/connection/common.py
@@ -4,7 +4,9 @@ class ConnectionInterface(object):
         """
         The init stuff comes here
         """
-
+        self.logger = logger.Logger().logger
+        self.audit_logger = logger.AuditLogger()
+    
     def load_server_modules(self):
         """
         In that module you will be loading the modules
@@ -33,16 +35,26 @@ class QpidConnection(ConnectionInterface):
 
 
 CERTMASTER_CONFIG = "/etc/certmaster/certmaster.conf"
-from config import read_config
-def choose_current_connection(conf_file):
+from certmaster.commonconfig import CMConfig
+from certmaster.config import read_config
+
+def choose_current_connection(conf_file=None,force_connection=None,cm_instance=None,port=None,listen_addr=None):
     """
     Choses the right connection from conf file and starts
     it according to that stuff ....
     """
-
     from certmaster.connection.xmlrpc.connection import XmlRpcConnection
     #from certmaster.connection.qpid.connection import XmlRpcConnection
-    config = read_config(CERTMASTER_CONFIG, CMConfig)
+    if not conf_file:
+        config = read_config(CERTMASTER_CONFIG, CMConfig)
+    else:
+        config = read_config(conf_file, CMConfig)
+    
+    #sometimes you may need passing the connection yourself
+    if force_connection:
+        print "Forcing the connection "
+        return force_connection(certmaster=cm_instance,port=port,listen_addr=listen_addr)
+
     connection = config.connection
     if not connection:
         connection = "xmlrpc"
