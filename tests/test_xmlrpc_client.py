2bc727272e141578423d83c66779682d252e1c5b
makkalot <makkalot@fedorabig.localdomain>
2009-05-29 15:49:21 +0300

    tests for unified part

--- tests/test_xmlrpc_client.py
+++ tests/test_xmlrpc_client.py
@@ -1,9 +1,13 @@
-import xmlrpclib
+from certmaster.connection.common import choose_current_client
+from certmaster.connection.xmlrpc.connection import XmlRpcClient
 
 MASTER_URI = "http://localhost:55555"
 def test_xml_rpc():
-    s = xmlrpclib.ServerProxy(MASTER_URI)
+    s = choose_current_client(master_uri = MASTER_URI,force_connection=XmlRpcClient)
     # print "DEBUG: waiting for cert"
     for i in range(100):
-        print i
         assert i == s.echo_me(i)
+ 
+    for i in ["One","two","three"]:
+        assert i == s.echo_me(i)
+
