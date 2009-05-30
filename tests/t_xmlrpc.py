e18515952ad481939dd16e47570f8b982d13b408
makkalot <makkalot@fedorabig.localdomain>
2009-05-30 21:38:04 +0300

    test cases for qpid will need to separate em of course

--- tests/t_xmlrpc.py
+++ tests/t_xmlrpc.py
@@ -1,9 +1,12 @@
 from certmaster.connection.xmlrpc.connection import XmlRpcConnection
+from certmaster.connection.qpid.server import QpidRpcCertMaster
 from certmaster.certmaster import CertMaster
 from certmaster.connection.common import choose_current_connection
 
 DEFAULT_TEST_PORT = 55555
 DEFAULT_TEST_ADRR = "localhost"
+
+SERVER_QUEUE = DEFAULT_TEST_ADRR
 
 CERTMASTER_CONFIG = "/etc/certmaster/certmaster.conf"
 class SomeCustomCertmaster(CertMaster):
@@ -18,7 +21,19 @@ class SomeCustomCertmaster(CertMaster):
 
 
 if __name__ == "__main__":
-    conn=choose_current_connection(force_connection=XmlRpcConnection,cm_instance=SomeCustomCertmaster(),port=DEFAULT_TEST_PORT,listen_addr=DEFAULT_TEST_ADRR)
-    conn.start_serving()
+    import sys
+
+    if sys.argv[1] == "xmlrpc":
+        print "Starting the xmlrpc server"
+        conn=choose_current_connection(force_connection=XmlRpcConnection,cm_instance=SomeCustomCertmaster(),port=DEFAULT_TEST_PORT,listen_addr=DEFAULT_TEST_ADRR)
+        conn.start_serving()
+    elif sys.argv[1]=="qpid":
+        print "Starting the qpid server"
+        conn=choose_current_connection(force_connection=QpidRpcCertMaster,cm_instance=SomeCustomCertmaster(),server_queue=SERVER_QUEUE)
+        conn.start_serving()
+    else:
+        print "No option like that exists"
+        
+
 
 
--- tests/test_xmlrpc_client.py
+++ tests/test_xmlrpc_client.py
@@ -1,9 +1,19 @@
 from certmaster.connection.common import choose_current_client
 from certmaster.connection.xmlrpc.connection import XmlRpcClient
+from certmaster.connection.qpid.client import QpidRpcClient
 
 MASTER_URI = "http://localhost:55555"
-def test_xml_rpc():
+def xml_rpc():
     s = choose_current_client(master_uri = MASTER_URI,force_connection=XmlRpcClient)
+    # print "DEBUG: waiting for cert"
+    for i in range(100):
+        assert i == s.echo_me(i)
+ 
+    for i in ["One","two","three"]:
+        assert i == s.echo_me(i)
+
+def test_qpid_rpc():
+    s = choose_current_client(queue="localhost",force_connection=QpidRpcClient)
     # print "DEBUG: waiting for cert"
     for i in range(100):
         assert i == s.echo_me(i)
